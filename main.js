d6555eee83a7526474193cfca66290fd28cf502b
Ryan <ryan@thecpafirm.com>
2020-12-17 22:35:26 -0700

    umm a lot

--- main.js
+++ main.js
@@ -32,17 +32,101 @@ let deleteFromDB = async (value) =>{
 let citySearchIn = document.querySelector('#in1')
 let citySearchSub = document.querySelector('#in2')
 
-citySearchSub.addEventListener('click', () => {
+citySearchSub.addEventListener('click', async () => {
     let searchData1 = citySearchIn.value
     console.log(`User Input data ${searchData1}`)
     ApiQuery.testImport()
 
-    let locactionSRes = ApiQuery.locationsSearch(searchData1)
-    // let locactionSRes = ApiQuery.citySearch(searchData1)
-    console.log(`Location Search Results: ${locactionSRes}`)
-    console.log(locactionSRes)
+    let locationSRes = await ApiQuery.locationsSearch(searchData1)
+    console.log(`Location Search Results: ${locationSRes}`)
+    console.log(locationSRes.location_suggestions)
 
+    renderCitySearch(locationSRes.location_suggestions)
 })
+
+let renderCitySearch = (sRes) =>{
+    let outDest = document.querySelector('#outDest')
+    outDest.innerHTML = ''
+
+    sRes.map((element, i) =>{
+        outDest.innerHTML += `
+            <div class='waves-effect displayCard dcAction' value='${i}' value1='${element.city_name}' value2='${element.entity_type}' value3='${element.city_id}' value4='${element.entity_id}' id='dca${i}'>
+                    <h5>${element.city_name}.</h5>
+            </div>`
+    })
+
+    document.querySelectorAll('.dcAction').forEach( element =>{
+        element.addEventListener( 'click', ()=>{
+            let itemSelector1 = element.getAttribute('value1')
+            let itemSelector2 = element.getAttribute('value2')
+            let itemSelector3 = element.getAttribute('value3')
+            let itemSelector4 = element.getAttribute('value4')
+            console.log(itemSelector1)
+            console.log(itemSelector2)
+            console.log(itemSelector3)
+            console.log(itemSelector4)
+            
+            lo_detailsSearch(itemSelector4, itemSelector2)
+        })
+    })
+}
+
+let lo_detailsSearch = async (ent_id, entity_type) =>{
+    let loDetails = await ApiQuery.location_detailsSearch(ent_id, entity_type)
+
+    let top10 = loDetails.best_rated_restaurant
+    let topC = loDetails.top_cuisines
+
+    let outDest = document.querySelector('#outDest')
+    outDest.innerHTML = ''
+
+    console.log(top10)
+    let topRestaurants = restaurantRender(top10)
+    let topCuisinesRes = `<h3 class='secHeader'>Top Local Cuisines</h3>`
+    topCuisinesRes += restaurantCuisines(topC)
+
+    outDest.innerHTML = topRestaurants
+    outDest.innerHTML += topCuisinesRes
+}
+
+let restaurantCuisines = (top5) =>{
+    let htmlReturn = ''
+    top5.map((element, i) =>{
+        htmlReturn += `
+        <div class='waves-effect displayCard dcAction' value='${i}' id='dca${i}'>
+            <h5>${element}</h5>
+        </div>`
+    })
+    return htmlReturn
+}
+
+let restaurantRender = (rArray) =>{
+    let htmlReturn = ''
+    htmlReturn += `<h3 class='secHeader'>Top Restaurants</h3>`
+    rArray.map((element, i) =>{
+        console.log(element.restaurant)
+        htmlReturn += `
+            <div class='waves-effect displayCard dcAction' value='${i}' id='dca${i}'>
+                <h5>${element.restaurant.name}.</h5>
+                <p>Cuisine: ${element.restaurant.cuisines}</p>
+                <br>
+                <p>Rating (${element.restaurant.all_reviews_count}):</p>
+                <p>${element.restaurant.user_rating.rating_text}: ${element.restaurant.user_rating.aggregate_rating}</p>
+                <br>
+                <p>Average cost for two: $${element.restaurant.average_cost_for_two}</p>
+                <br>
+                <p>Delivering now: ${element.restaurant.is_delivering_now ? 'Yes' : 'No'}</p>
+                <br>
+                <p>Phone:</p>
+                <p>${element.restaurant.phone_numbers}</p>
+                <br>
+                <p>Address: </p>
+                <p>${element.restaurant.location.address}</p>
+            </div>`
+    })
+
+    return htmlReturn
+}
 
 // Convenience feature, submits search when enter is pressed from target search input field
 citySearchIn.addEventListener('keyup', (event) =>{
--- zomatoConnect.js
+++ zomatoConnect.js
@@ -17,21 +17,21 @@
 //      res_id
 // 
 
-// Jim is looking for a place to eat in a city he's near while driving cross country.
-// Eric and Erin are trying to find somewhere different to eat than the normal options they tend to go for.
-// Cam, Le Su, Jeff, Jenny, and Pam are looking for a place to eat for their ethnic food Friday tradition but can't decide so just want something that will give them a random restaurant in their area.
-// Chuck wants to find a highly rated pizzeria
-// Jenna and Zelda want to find a good restaurant in a certain price range
-// Mark wants to find a place to get coffee for one.
-// Mily and Sahra are looking for a good bar in the area
-// Derick and Dale are looking for the cheapest bar in their town
-// André is trying to figure out how many restaurants are in his zipcode
-// Bill is tired of searching for restaurants on his map app only to have that force him out of that app and into another to view images and so is protesting searching for restaurants on it due to personal moral qualms.
-// Hilda is in charge of finding a dinner location for her group
-// Hans is so hipster that he only uses obscure non mainstream apps to find the best hip undiscovered food joints.
-// May,  June, and April are trying to find out which café has the most reviews
-// Mark works for Zomato and needs to search for the number of restaurants Zomato has registered by city
-// Jerry just wants a place that delivers right now.
+// Jim is looking for a place to eat in a city he's near while driving cross country. *
+// Eric and Erin are trying to find somewhere different to eat than the normal options they tend to go for. */
+// Cam, Le Su, Jeff, Jenny, and Pam are looking for a place to eat for their ethnic food Friday tradition but can't decide so just want something that will give them a random restaurant in their area. */
+// Chuck wants to find a highly rated pizzeria 
+// Jenna and Zelda want to find a good restaurant in a certain price range *
+// Mark wants to find a place to get coffee for one. 
+// Mily and Sahra are looking for a good bar in the area 
+// Derick and Dale are looking for the cheapest bar in their town 
+// André is trying to figure out how many restaurants are in his zipcode X
+// Bill is tired of searching for restaurants on his map app only to have that force him out of that app and into another to view images and so is protesting searching for restaurants on it due to personal moral qualms. X
+// Hilda is in charge of finding a dinner location for her group *
+// Hans is so hipster that he only uses obscure non mainstream apps to find the best hip undiscovered food joints. *
+// May,  June, and April are trying to find out which café has the most reviews 
+// Mark works for Zomato and needs to search for the number of restaurants Zomato has registered by city *
+// Jerry just wants a place that delivers right now. *
 
 const apiQuery = {
     url: 'https://developers.zomato.com/api/v2.1',
@@ -93,30 +93,47 @@ const apiQuery = {
         return searchCityRes
     },
 
+    // locationsSearch: async (locationVar) =>{
+    //     let methodURL = apiQuery.url + `/locations?query=${locationVar}`
+    //     let searchCity = await fetch(methodURL,{
+    //         method: 'GET',
+    //         headers: { 'user-key': apiQuery.melon}
+    //     })
+    //     console.log('This is the searchCity Results: ')
+    //     console.log(searchCity)
+    //     let searchCityRes = await searchCity.json()
+    //     console.log('This is the searchCityRes (Post .json()): ')
+    //     console.log(searchCityRes)
+    //     console.log('This is the .location_suggestions (Post .json()): ')
+    //     console.log(searchCityRes.result)
+    //     return searchCityRes
+    // },
     locationsSearch: async (locationVar) =>{
-        let methodURL = apiQuery.url + `/locationsSearch?inQ=${locationVar}`
+        let methodURL = apiQuery.url + `/locations?query=${locationVar}&count=8`
         let searchCity = await fetch(methodURL,{
             method: 'GET',
             headers: { 'user-key': apiQuery.melon}
         })
         console.log('This is the searchCity Results: ')
         console.log(searchCity)
-        let searchCityRes = searchCity.json()
+        let searchCityRes = await searchCity.json()
         console.log('This is the searchCityRes (Post .json()): ')
         console.log(searchCityRes)
         return searchCityRes
     },
 
-    location_detailsSearch: async (entityID) =>{
-        let methodURL = this.url + `/location_details?entity_id=${entityID}`
+    location_detailsSearch: async (entityID, entityType) =>{
+        console.log(entityID)
+        console.log(entityType)
+        let methodURL = apiQuery.url + `/location_details?entity_id=${entityID}&entity_type=${entityType}`
         let searchCity = await fetch(methodURL,{
             method: 'GET',
-            headers: { 'user-key': Z_CONNECT}
+            headers: { 'user-key': apiQuery.melon}
         })
-        console.log('This is the searchCity Results: ')
+        console.log('This is the location detail search Results: ')
         console.log(searchCity)
-        let searchCityRes = searchCity.json()
-        console.log('This is the searchCityRes (Post .json()): ')
+        let searchCityRes = await searchCity.json()
+        console.log('This is the Location Detail Search (Post .json()): ')
         console.log(searchCityRes)
         return searchCityRes
     },
